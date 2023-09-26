6708c5c67a612b671a76f6ba16c7870ff98c09c0
Jörgen Brandt <joergen@cuneiform-lang.org>
2023-09-26 22:01:06 +0200

    fix version

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/mqtt-client.scrbl" ())))
 (define pkg-desc "Description Here")
-(define version "0.1.0")
+(define version "0.1")
 (define pkg-authors '("Jörgen Brandt"))
 (define license '(Apache-2.0))
